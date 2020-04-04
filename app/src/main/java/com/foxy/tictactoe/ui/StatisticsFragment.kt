55df381f8d5a6e66201de8fdbcb1483b89742c47
Factis <53155715qwerty>
2020-04-04 17:04:58 +0800

    save win statistics

--- app/src/main/java/com/foxy/tictactoe/ui/StatisticsFragment.kt
+++ app/src/main/java/com/foxy/tictactoe/ui/StatisticsFragment.kt
@@ -8,6 +8,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.findNavController
 import com.foxy.tictactoe.R
+import com.foxy.tictactoe.utils.getPvAHardStatistics
+import com.foxy.tictactoe.utils.getPvALazyStatistics
+import com.foxy.tictactoe.utils.getPvPStatistics
+import kotlinx.android.synthetic.main.fragment_statistics.*
 import moxy.MvpAppCompatFragment
 
 class StatisticsFragment : MvpAppCompatFragment() {
@@ -20,6 +24,20 @@ class StatisticsFragment : MvpAppCompatFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar()
+        setupStatistics()
+    }
+
+    private fun setupStatistics() {
+        val pvp = getPvPStatistics()
+        val pvaLazy = getPvALazyStatistics()
+        val pvaHard = getPvAHardStatistics()
+
+        tv_pvp_x.text = pvp.first.toString()
+        tv_pvp_o.text = pvp.second.toString()
+        tv_pva_lazy_x.text = pvaLazy.first.toString()
+        tv_pva_lazy_o.text = pvaLazy.second.toString()
+        tv_pva_hard_x.text = pvaHard.first.toString()
+        tv_pva_hard_o.text = pvaHard.second.toString()
     }
 
     private fun returnToStartFragment() {
--- app/src/main/java/com/foxy/tictactoe/utils/PrefUtils.kt
+++ app/src/main/java/com/foxy/tictactoe/utils/PrefUtils.kt
@@ -1,17 +1,76 @@
 package com.foxy.tictactoe.utils
 
+import android.content.Context
 import androidx.preference.PreferenceManager
 import com.foxy.tictactoe.App
 
+// For settings
 private const val GAME_MODE = "prefs_game_mode"
 private const val GAME_MODE_DEFAULT = "Player vs Player"
 private const val FIELD_SIZE = "prefs_field_size"
 private const val FIELD_SIZE_DEFAULT = 3
 
-private val prefs by lazy {
+// For statistics
+private const val PVP_PLAYER_X = "statistics_pvp_player_x"
+private const val PVP_PLAYER_O = "statistics_pvp_player_o"
+private const val PVA_LAZY_PLAYER_X = "statistics_pva_lazy_player_x"
+private const val PVA_LAZY_PLAYER_O = "statistics_pva_lazy_player_o"
+private const val PVA_HARD_PLAYER_X = "statistics_pva_hard_player_x"
+private const val PVA_HARD_PLAYER_O = "statistics_pva_hard_player_o"
+
+private val prefsSettings by lazy {
     PreferenceManager.getDefaultSharedPreferences(App.get())
 }
 
-fun getGameMode() : String = prefs.getString(GAME_MODE, GAME_MODE_DEFAULT)!!
+private val prefsStatistics by lazy {
+    App.get().getSharedPreferences("com.foxy.tictactoe_prefs_statistics", Context.MODE_PRIVATE)
+}
 
-fun getFieldSize() : Int = prefs.getInt(FIELD_SIZE, FIELD_SIZE_DEFAULT)
+fun getGameMode() : String = prefsSettings.getString(GAME_MODE, GAME_MODE_DEFAULT)!!
+
+fun getFieldSize() : Int = prefsSettings.getInt(FIELD_SIZE, FIELD_SIZE_DEFAULT)
+
+fun savePvPStatistics(playerX: Boolean) {
+    val pvp = getPvPStatistics()
+    if (playerX) {
+        prefsStatistics.edit().putInt(PVP_PLAYER_X, pvp.first + 1).apply()
+    } else {
+        prefsStatistics.edit().putInt(PVP_PLAYER_O, pvp.second + 1).apply()
+    }
+}
+
+fun getPvPStatistics(): Pair<Int, Int> {
+    val x = prefsStatistics.getInt(PVP_PLAYER_X, 0)
+    val o = prefsStatistics.getInt(PVP_PLAYER_O, 0)
+    return Pair(x, o)
+}
+
+fun savePvALazyStatistics(playerX: Boolean) {
+    val pvaLazy = getPvALazyStatistics()
+    if (playerX) {
+        prefsStatistics.edit().putInt(PVA_LAZY_PLAYER_X, pvaLazy.first + 1).apply()
+    } else {
+        prefsStatistics.edit().putInt(PVA_LAZY_PLAYER_O, pvaLazy.second + 1).apply()
+    }
+}
+
+fun getPvALazyStatistics(): Pair<Int, Int> {
+    val x = prefsStatistics.getInt(PVA_LAZY_PLAYER_X, 0)
+    val o = prefsStatistics.getInt(PVA_LAZY_PLAYER_O, 0)
+    return Pair(x, o)
+}
+
+fun savePvAHardStatistics(playerX: Boolean) {
+    val pvaHard = getPvAHardStatistics()
+    if (playerX) {
+        prefsStatistics.edit().putInt(PVA_HARD_PLAYER_X, pvaHard.first + 1).apply()
+    } else {
+        prefsStatistics.edit().putInt(PVA_HARD_PLAYER_O, pvaHard.second + 1).apply()
+    }
+}
+
+fun getPvAHardStatistics(): Pair<Int, Int> {
+    val x = prefsStatistics.getInt(PVA_HARD_PLAYER_X, 0)
+    val o = prefsStatistics.getInt(PVA_HARD_PLAYER_O, 0)
+    return Pair(x, o)
+}
